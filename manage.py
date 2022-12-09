2e51b228050c08219bd5a2d335f4b5216b5407be
Dan F <dfrankow@gmail.com>
2022-12-09 08:25:03 -0600

    Some nice things (#1)
    * .gitignore
    * pre-commit
    * github checks
    * Move 'ai_coach' app up one level
    * Add a stub for a test

--- manage.py
+++ manage.py
@@ -8,12 +8,13 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_coach.settings')
     try:
+        # pylint: disable-next=import-outside-toplevel
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            'available on your PYTHONPATH environment variable? Did you '
+            'forget to activate a virtual environment?'
         ) from exc
     execute_from_command_line(sys.argv)
 
