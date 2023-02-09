1623b85ed2b7ed01a7935c764dfa7b3da0e01be6
Michael Cassano <mcassano@gmail.com>
2023-02-09 10:03:50 -0700

    Revert "introduce measurement_type on Exercise (#13)"
    This reverts commit 48136577ed09483294be7294275f8e6332377aae.

--- web/models.py
+++ web/models.py
@@ -5,30 +5,14 @@ from accounts.models import CustomUser
 class Exercise(models.Model):
     PUSHUP = 'Push-up'
     FLAPPINGCROSS = 'Flapping-Cross'
-    PLANK = 'Plank'
     EXERCISE_CHOICES = [
         (PUSHUP, PUSHUP),
         (FLAPPINGCROSS, FLAPPINGCROSS),
-        (PLANK, PLANK)
     ]
-
-    SECOND = 'Second'
-    CYCLE = 'Cycle'
-    MEASUREMENT_TYPE_CHOICES = [
-        (SECOND, SECOND),
-        (CYCLE, CYCLE),
-    ]
-
     name = models.CharField(
         max_length=30,
         choices=EXERCISE_CHOICES,
         default=PUSHUP,
-    )
-
-    measurement_type = models.CharField(
-        max_length=30,
-        choices=MEASUREMENT_TYPE_CHOICES,
-        default=CYCLE,
     )
 
     def __str__(self):
@@ -37,12 +21,12 @@ class Exercise(models.Model):
 
 class ExerciseSet(models.Model):
     datetime_performed = models.DateTimeField()
-    measurement = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    num_reps = models.IntegerField()
     performed_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     exercise_performed = models.ForeignKey(Exercise, on_delete=models.CASCADE)
 
     def __str__(self):
         return (f'{self.performed_by} did '
                 f'{self.exercise_performed.name} '
-                f'{self.measurement} {self.exercise_performed.measurement_type} on '
+                f'{self.num_reps} time(s) on '
                 f'{self.datetime_performed}')
