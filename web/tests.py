8842a8a35c7997c5815e5d98d183a8570ca3292c
Michael Cassano <mcassano@gmail.com>
2023-05-26 16:42:22 -0600

    add duration and target reps

--- web/tests.py
+++ web/tests.py
@@ -17,9 +17,11 @@ class ExerciseSetTestCase(TestCase):
 
         ExerciseSet.objects.create(datetime_performed=timezone.now(),
                                    num_reps=10,
+                                   num_target_reps=10,
                                    performed_by=exerciser,
                                    exercise_performed=push_up,
-                                   duration_seconds=34)
+                                   duration_seconds=34,
+                                   duration_target_seconds=0)
 
         actual = ExerciseSet.objects.get(performed_by=exerciser)
         self.assertEqual(10, actual.num_reps)
@@ -38,9 +40,11 @@ class TestExerciseSetViews(TestCase):
         self.exercise_set = ExerciseSet.objects.create(
             datetime_performed=timezone.now(),
             num_reps=10,
+            num_target_reps =10,
             performed_by=user,
             exercise_performed=self.exercise,
-            duration_seconds=50)
+            duration_seconds=50,
+            duration_target_seconds=0)
 
     def test_exercise_set_can_be_GET(self):
         """get a users single exercise set"""
@@ -56,15 +60,21 @@ class TestExerciseSetViews(TestCase):
                          response.data[0]["performed_by"]["username"])
         self.assertEqual(self.exercise_set.num_reps,
                          response.data[0]["num_reps"])
+        self.assertEqual(self.exercise_set.num_target_reps,
+                         response.data[0]["num_target_reps"])
         self.assertEqual(self.exercise_set.duration_seconds,
                          response.data[0]["duration_seconds"])
+        self.assertEqual(self.exercise_set.duration_target_seconds,
+                         response.data[0]["duration_target_seconds"])
 
     def test_exercise_set_can_be_POST(self):
         """post an exercise set to a user"""
         the_post_data = {"exercise_performed": {"name": "Push-up"},
                          "datetime_performed": "2023-01-04T04:20:27Z",
                          "num_reps": 10,
-                         "duration_seconds": 12}
+                         "num_target_reps": 10,
+                         "duration_seconds": 12,
+                         "duration_target_seconds": 0}
         response = self.client.post(path='/exercise-sets/',
                                     data=the_post_data,
                                     content_type='application/json',
@@ -74,6 +84,8 @@ class TestExerciseSetViews(TestCase):
                          response.status_code)
         self.assertEqual(10,
                          response.data["num_reps"])
+        self.assertEqual(10,
+                         response.data["num_target_reps"])
         self.assertEqual('Push-up',
                          response.data["exercise_performed"]["name"])
         self.assertEqual('tom',
@@ -82,3 +94,5 @@ class TestExerciseSetViews(TestCase):
                          response.data["datetime_performed"])
         self.assertEqual(12,
                          response.data["duration_seconds"])
+        self.assertEqual(0,
+                         response.data["duration_target_seconds"])
